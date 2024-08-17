e99d11a6cfb456f5ad328022699aa0c263772f43
Ruslan-Suvorov <bloodsrv@gmail.com>
2024-08-17 12:31:51 +0300

    fix style

--- src/page/Home.js
+++ src/page/Home.js
@@ -95,7 +95,7 @@ const Home = () => {
               )}
               <div style={{ minWidth: "300px" }}>
                 {tag && (
-                  <p style={{ marginTop: "-15px" }}>
+                  <p>
                     Filtered by Tag{" "}
                     <span
                       className="text-start"
